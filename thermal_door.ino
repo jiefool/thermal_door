a30298d546ec3ed30ae5d2b568a2762e1b9e5411
Jay Paul Aying <jay@blendit.eu>
2021-04-26 23:42:01 +0800

    thermal door

--- thermal_door.ino
+++ thermal_door.ino
@@ -1,9 +1,68 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_MLX90614.h>
 
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+float temp=0.0;
+int openDoor = 2;
+int closeDoor = 3;
+String doorStatus = "close";
+long actuatorDelay = 20000;
+
+void setup(){
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight(); 
+
+  pinMode(openDoor, OUTPUT);
+  pinMode(closeDoor, OUTPUT);
+
+  mlx.begin();
+
+  constantScreen();
+
+  closeDoorFunc();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  temp = mlx.readObjectTempC();
+  printToLCD(0,2, (String) temp + " C");
 
+  if(temp > 32 && temp < 37){
+    openDoorFunc();
+    printToLCD(0,3, "Door Status: "+doorStatus+"  ");
+  }else{
+    closeDoorFunc(); 
+    printToLCD(0,3, "Door Status: "+doorStatus+"  ");
+  }
+  
+  delay(100);
 }
+
+
+void constantScreen(){
+  printToLCD(0,0, "Thermal Door System");
+  printToLCD(0,1, "Reading Temperature:");
+  printToLCD(0,2, (String) temp + " C");
+  printToLCD(0,3, "Door Status: close");
+}
+
+void printToLCD(int x, int y, String text){
+  lcd.setCursor(x,y);
+  lcd.print(text);
+}
+
+void openDoorFunc(){
+     digitalWrite(closeDoor, HIGH);
+     digitalWrite(openDoor, LOW);
+     doorStatus = "open";
+}
+
+void closeDoorFunc(){
+   digitalWrite(closeDoor, LOW);
+   digitalWrite(openDoor, HIGH);
+   doorStatus = "close";
+}
+   
